37adc35c94afded08562bce455211ff0adf95637
Amber <amber@transcental.dev>
2025-02-18 21:37:20 +0000

    oops

--- flowey/utils/messager.py
+++ flowey/utils/messager.py
@@ -10,6 +10,7 @@ async def message(user_id: str):
                 channel=user_id,
                 text="Howdy! I'm Flowey. Flowey the Flower!\n\nAs one of TORIEL's closest friends, I'm here to help you get upgraded to a full member now that the High Seas have drained leaving an empty backroom behind.\n*Stumble into <#C039PAG1AV7If> to continue.* If you get stuck, feel free to message <@U054VC2KM9P> or email slack@hackclub.com!"
             )
+            done = True
         except SlackApiError as e:
             if e.response["error"] == "ratelimited":
                 retry_after = int(e.response.headers.get("Retry-After", 1))
