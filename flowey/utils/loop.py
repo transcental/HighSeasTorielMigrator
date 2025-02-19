19e11da90042c1a680fbb2ff9f0dc1eb8dc02135
Amber <amber@transcental.dev>
2025-02-19 13:30:38 +0000

    fix the incorrect channel id

--- flowey/utils/loop.py
+++ flowey/utils/loop.py
@@ -3,7 +3,7 @@ from pyairtable import Api
 import logging
 from flowey.utils.env import env
 from flowey.utils.inviter import invite
-from flowey.utils.messager import message
+from flowey.utils.messager import fix_message, message
 
 ac = Api(
     api_key=env.airtable_api_key,
@@ -13,26 +13,39 @@ ac = Api(
 ut = ac.table(env.airtable_base_id, env.airtable_table_id)
 
 async def check_users():
-    users = ut.all(view="[FLOWEY] Non-migrated users", fields=["slack_id"])
+    users = ut.all(view="[FLOWEY] Non-migrated users", fields=["slack_id", "migrated_to_toriel", "sent_fixed_flowey_message"])
     logging.info(f"Checking {len(users)} users")
     for user in users:
         logging.info(f"Checking {user['id']}")
         slack_id = user.get("fields", {}).get("slack_id", "")
+        migrated = user.get("fields", {}).get("migrated_to_toriel", False)
+        sent_fixed = user.get("fields", {}).get("sent_fixed_flowey_message", False)
         if not slack_id:
             logging.info("No slack_id found for user", user['id'])
             continue
-        invited = await invite(slack_id)
-        if invited:
-            logging.info(f"Invited {slack_id}, messaging")
-            await message(slack_id)
-        else:
-            logging.info(f"Failed to invite {slack_id}")
-        while True:
-            try:
-                ut.update(user["id"], {"migrated_to_toriel": True})
-                break
-            except:
-                logging.error("Failed to update user", user["id"])
-                await asyncio.sleep(0.5)
-        logging.info(f"Migrated {slack_id}")
+        if not migrated:
+            invited = await invite(slack_id)
+            if invited:
+                logging.info(f"Invited {slack_id}, messaging")
+                await message(slack_id)
+            else:
+                logging.info(f"Failed to invite {slack_id}")
+            while True:
+                try:
+                    ut.update(user["id"], {"migrated_to_toriel": True, "sent_fixed_flowey_message": True})
+                    break
+                except:
+                    logging.error("Failed to update user", user["id"])
+                    await asyncio.sleep(0.5)
+            logging.info(f"Migrated {slack_id}")
+        elif migrated and not sent_fixed:
+            await fix_message(slack_id)
+            while True:
+                try:
+                    ut.update(user["id"], {"sent_fixed_flowey_message": True})
+                    break
+                except:
+                    logging.error("Failed to update user", user["id"])
+                    await asyncio.sleep(0.5)
+                logging.info(f"Fixed message for {slack_id}")
         await asyncio.sleep(1)
--- flowey/utils/messager.py
+++ flowey/utils/messager.py
@@ -8,7 +8,25 @@ async def message(user_id: str):
         try:
             await env.slack_client.chat_postMessage(
                 channel=user_id,
-                text="Howdy! I'm Flowey. Flowey the Flower!\n\nAs one of TORIEL's closest friends, I'm here to help you get upgraded to a full member now that the High Seas have drained leaving an empty backroom behind.\n*Stumble into <#C039PAG1AV7If> to continue.* If you get stuck, feel free to message <@U054VC2KM9P> or email slack@hackclub.com!"
+                text="Howdy! I'm Flowey. Flowey the Flower!\n\nAs one of TORIEL's closest friends, I'm here to help you get upgraded to a full member now that the High Seas have drained leaving an empty backroom behind.\n*Stumble into <#C039PAG1AV7> to continue.* If you get stuck, feel free to message <@U054VC2KM9P> or email slack@hackclub.com!"
+            )
+            done = True
+        except SlackApiError as e:
+            if e.response["error"] == "ratelimited":
+                retry_after = int(e.response.headers.get("Retry-After", 1))
+                await asyncio.sleep(retry_after)
+            else:
+                raise e
+                done = True
+            continue
+
+async def fix_message(user_id: str):
+    done = False
+    while not done:
+        try:
+            await env.slack_client.chat_postMessage(
+                channel=user_id,
+                text="Howdy! It looks like Flowey did a goof and linked the wrong channel!.\nYou should *stumble into <#C039PAG1AV7> to continue.* If you get stuck, please message <@U054VC2KM9P> or email slack@hackclub.com!"
             )
             done = True
         except SlackApiError as e:
