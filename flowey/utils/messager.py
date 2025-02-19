87eb7ed78ccf507c9126da6ab5e48dd8038dbc8c
Amber <amber@transcental.dev>
2025-02-19 13:41:59 +0000

    oopsies, i should check if they're an MCG

--- flowey/utils/messager.py
+++ flowey/utils/messager.py
@@ -1,4 +1,6 @@
 import asyncio
+import logging
+import traceback
 from slack_sdk.errors import SlackApiError
 from flowey.utils.env import env
 
@@ -21,6 +23,35 @@ async def message(user_id: str):
             continue
 
 async def fix_message(user_id: str):
+    completed = False
+    while not completed:
+        try:
+            user_info = await env.slack_client.users_info(user=user_id)
+            mcg: bool = user_info["user"].get("is_restricted", False)
+            if not mcg:
+                return False
+            completed = True
+        except SlackApiError as e:
+            if e.response["error"] == "ratelimited":
+                logging.info("User info ratelimited")
+                retry_after = int(e.response.headers.get("Retry-After", 1))
+                await asyncio.sleep(retry_after)
+            else:
+                t_info = traceback.format_exc()
+                logging.error(f"Failed to get user info: {e}", exc_info=True)
+                while True:
+                    try:
+                        await env.slack_client.chat_postMessage(channel="U054VC2KM9P", text=f"Failed to get user info: {e}\n```{t_info}```")
+                        break
+                    except SlackApiError as ef:
+                        if e.response["error"] == "ratelimited":
+                            retry_after = int(e.response.headers.get("Retry-After", 1))
+                            await asyncio.sleep(retry_after)
+                        else:
+                            logging.error(f"Failed to send error message: {ef}", exc_info=True)
+                            break
+                completed = True
+                return False
     done = False
     while not done:
         try:
